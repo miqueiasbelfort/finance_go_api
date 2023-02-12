add605f8c3fe99c0ea9a1db6176fc80ce6b1fbb1
miqueiasbelfort <kawasousa1554@gmail.com>
2023-02-12 20:31:25 -0300

    token

--- src/authentication/token.go
+++ src/authentication/token.go
@@ -1,6 +1,10 @@
 package authentication
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -15,5 +19,40 @@ func CreateAToken(userID primitive.ObjectID) (string, error) {
 	permissions["userID"] = userID
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
-	return token.SignedString([]byte("Heloo World"))
+	return token.SignedString([]byte("123456789"))
+}
+
+// get the token and verify if the token is valid
+func ValidateToken(r *http.Request) error {
+	tokenString := getAToken(r)
+	token, err := jwt.Parse(tokenString, getAVerificationKey)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("Token is Invalid")
+
+}
+
+// Get the token by request
+func getAToken(r *http.Request) string {
+
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func getAVerificationKey(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Unexpected signature method! %v", token.Header["alg"])
+	}
+	return "123456789", nil
 }
